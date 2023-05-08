e12ae6a02ea3a910824bf6d099768b8cf686304c
Jardak <kutalek@gymlit.cz>
2023-05-08 08:07:40 +0200

    Synchronizace z 2.5.2023

--- tlacitko.ino
+++ tlacitko.ino
@@ -1,10 +1,10 @@
 
 
-
+const byte led = 13, led1 = 12, tlacitko = 11;
 
 int zpozdeni = 400;
 
-const byte led = 13, led1 = 12, tlacitko = 11;
+
 int cteni, cteniOld=0,  pocetpolozek;
 unsigned long casMic, casMicOld, casRozdil;
 
@@ -20,7 +20,7 @@ void setup()
 
 void loop() 
 {
-  casMicOld = casMic;
+  
   cteniOld = cteni;
   cteni= digitalRead(tlacitko);
   casMic = millis();
@@ -28,15 +28,52 @@ void loop()
   if(cteni!=cteniOld)
   {
     casRozdil= casMic - casMicOld;
-    if(pocetpolozek++>30) 
+    
+    
+    
+    if (casRozdil>80) {
+
+      tisk();
+      casMicOld = casMic;
+
+    }
+
+    
+  }
+
+led();
+
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+void tisk() 
+{
+if(pocetpolozek++>20) 
     {
 
       Serial.println();pocetpolozek = 0;
    
  
     }
-       Serial.print(cteni); Serial.print("-"); Serial.print(casRozdil); Serial.print(",");
-  }
+     Serial.print(cteni); Serial.print("-"); Serial.print(casRozdil); Serial.print(",");
+  
+
+
+
+}
+
+void led()
+{
 
 
   if(cteni == HIGH)
@@ -53,13 +90,12 @@ void loop()
   }
 
 
-  
 }
 
 
 
 
-void zobraz()
-{
 
-}
+
+
+
