92b00a5727bb3e52cbd7f6b37850ae30327c5e67
Jardak <kutalek@gymlit.cz>
2023-05-09 14:59:31 +0200

    Úkol na příště z  9.5.23

--- analog_vstup/analog_vstup.ino
+++ analog_vstup/analog_vstup.ino
@@ -1,8 +1,15 @@
-void setup() {
+// Zkopírujte minulý program, ať nezačínáte od nuly
+// nastavte parametry pro jednotlivé piny
+// 		analogový vstupní
+// 		digitální pro led červenou, žlutou a zelenou
 
+void setup() {
+// nastavte vstupní analogový pin a výstupní 
+// případně seriový port
 }
 
 void loop() {
- 
+ // načtěte analogRead(x)
+ // otestujeho velikost a rozsviťte odpovídající diodu
 
 }
