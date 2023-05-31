b121981f57fee46af9295733c295293ab106cfdc
Jardak <kutalek@gymlit.cz>
2023-05-31 11:12:17 +0200

    Práce ve škole 30.5.2023

--- analog_vstup/analog_vstup.ino
+++ analog_vstup/analog_vstup.ino
@@ -1,15 +1,230 @@
-// Zkopírujte minulý program, ať nezačínáte od nuly
-// nastavte parametry pro jednotlivé piny
-// 		analogový vstupní
-// 		digitální pro led červenou, žlutou a zelenou
 
-void setup() {
-// nastavte vstupní analogový pin a výstupní 
-// případně seriový port
+
+const byte led1 = 10, led2 = 8, led3 = 12;
+
+int zpozdeni = 400;
+
+
+int cteni, cteniOld=0,  pocetpolozek;
+unsigned long casMic, casMicOld, casRozdil;
+
+
+void setup() 
+{
+ pinMode(A0, INPUT);
+ pinMode(led1,OUTPUT);
+ pinMode(led2,OUTPUT);
+ pinMode(led3,OUTPUT);
+ Serial.begin(9600);
+ 
+ 
 }
 
-void loop() {
- // načtěte analogRead(x)
- // otestujeho velikost a rozsviťte odpovídající diodu
+void loop() 
+{
+  
+
+  analogRead(A0)
+  cteniOld = cteni;
+  cteni= digitalRead(tlacitko);
+  casMic = millis();
+  
+  if(cteni!=cteniOld)
+  {
+    casRozdil= casMic - casMicOld;
+    
+    
+    
+    if (casRozdil>80) {
+
+      tisk();
+      casMicOld = casMic;
+
+    }
+
+    
+  }
+
+if(cteni == HIGH)
+  {
+
+    digitalWrite(led1, HIGH);
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+
+    digitalWrite(led1,LOW);
+    digitalWrite(led,LOW);
+  }
+
+
+  
+}
+
+
+
+
+void tisk() 
+{
+if(pocetpolozek++>20) 
+    {
+
+      Serial.println();pocetpolozek = 0;
+   
+ 
+    }
+     Serial.print(cteni); Serial.print("-"); Serial.print(casRozdil); Serial.print(",");
+  
+
+
 
 }
+
+int cteni;
+int led = 13;
+int led1 = 12;
+
+void led()
+	// Předat všechny proměnné jako parametry led( int pin, int sta) - (proměnná ctení název neodpovídá obsahu)
+		// a ovládat jen jednu diodu
+		// název led - název neodpovídá obsahu např(LedSvitiAnoNe)
+{
+
+
+  if(cteni == HIGH)
+  {
+
+    digitalWrite(led1, HIGH);
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+
+    digitalWrite(led1,LOW);
+    digitalWrite(led,LOW);
+  }
+
+
+}
+
+
+
+const byte led = 13, led1 = 12, tlacitko = 11;
+
+int zpozdeni = 400;
+
+
+int cteni, cteniOld=0,  pocetpolozek;
+unsigned long casMic, casMicOld, casRozdil;
+
+void setup() 
+{
+ pinMode(tlacitko, INPUT);
+ pinMode(led,OUTPUT);
+ pinMode(led1,OUTPUT);
+ Serial.begin(9600);
+ 
+ 
+}
+
+void loop() 
+{
+  
+  cteniOld = cteni;
+  cteni= digitalRead(tlacitko);
+  casMic = millis();
+  
+  if(cteni!=cteniOld)
+  {
+    casRozdil= casMic - casMicOld;
+    
+    
+    
+    if (casRozdil>80) {
+
+      tisk();
+      casMicOld = casMic;
+
+    }
+
+    
+  }
+
+if(cteni == HIGH)
+  {
+
+    digitalWrite(led1, HIGH);
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+
+    digitalWrite(led1,LOW);
+    digitalWrite(led,LOW);
+  }
+
+
+  
+}
+
+
+
+
+void tisk() 
+{
+if(pocetpolozek++>20) 
+    {
+
+      Serial.println();pocetpolozek = 0;
+   
+ 
+    }
+     Serial.print(cteni); Serial.print("-"); Serial.print(casRozdil); Serial.print(",");
+  
+
+
+
+}
+
+int cteni;
+int led = 13;
+int led1 = 12;
+
+void led()
+	// Předat všechny proměnné jako parametry led( int pin, int sta) - (proměnná ctení název neodpovídá obsahu)
+		// a ovládat jen jednu diodu
+		// název led - název neodpovídá obsahu např(LedSvitiAnoNe)
+{
+
+
+  if(cteni == HIGH)
+  {
+
+    digitalWrite(led1, HIGH);
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+
+    digitalWrite(led1,LOW);
+    digitalWrite(led,LOW);
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
